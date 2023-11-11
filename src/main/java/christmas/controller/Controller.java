571e0dae3e2a9cb2b944db8df848c10bcaf0bceb
ay-eonii <naayen@naver.com>
2023-11-11 16:18:34 +0900

    feat(input): 날짜 입력 요청 메시지 출력

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -1,11 +1,15 @@
 package christmas.controller;
 
+import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class Controller {
     private OutputView outputView = new OutputView();
+    private InputView inputView = new InputView();
 
     public void start() {
         outputView.printWelcome();
+
+        inputView.readDate();
     }
 }
---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,8 @@
+package christmas.view;
+
+public class InputView {
+    private static final String INPUT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public void readDate() {
+        System.out.println(INPUT_DATE);
+    }
+}
