6dd8f0a465506aeaeddfbe8cdda1ab6d138e0b4a
ay-eonii <naayen@naver.com>
2023-11-11 16:19:49 +0900

    feat(input): 날짜 입력 받기

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,8 +1,11 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private static final String INPUT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public void readDate() {
         System.out.println(INPUT_DATE);
+        String input = Console.readLine();
     }
 }
