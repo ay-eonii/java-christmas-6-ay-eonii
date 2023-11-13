7be287ee80778898f6cd4f7eee82a96a38162c03
ay-eonii <naayen@naver.com>
2023-11-13 18:59:25 +0900

    test(presentation): 증정이벤트 기준 금액 테스트

--- src/test/java/christmas/domain/benefit/PresentationTest.java
+++ src/test/java/christmas/domain/benefit/PresentationTest.java
@@ -33,7 +33,7 @@ class PresentationTest {
         Assertions.assertEquals(actual, "샴페인 1개");
     }
 
-    @DisplayName("할인 전 총주문 금액이 120,000원 이하면 증정 이벤트에 해당하지 않는다.")
+    @DisplayName("할인 전 총주문 금액이 120,000원 미만이면 증정 이벤트에 해당하지 않는다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 119_999})
     void getNonePresentationMenu(int totalPrice) {
@@ -54,9 +54,28 @@ class PresentationTest {
         assertThat(actual).isTrue();
     }
 
+    @DisplayName("할인 전 총주문 금액이 120,000원 이상이면 25,000원 어치의 혜택을 받는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {120_000, 999_999})
+    void calculateBenefitOverStandard(int totalPrice) {
+        Order order = mock(Order.class);
+        when(order.getTotalPrice()).thenReturn(totalPrice);
 
-    @Test
-    void calculateBenefit() {
+        int actual = presentation.calculateBenefit(order);
+
+        assertThat(actual).isEqualTo(25000);
+    }
+
+    @DisplayName("할인 전 총주문 금액이 120,000원 미만이면 0원의 혜택을 받는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 119_999})
+    void calculateBenefitUnderStandard(int totalPrice) {
+        Order order = mock(Order.class);
+        when(order.getTotalPrice()).thenReturn(totalPrice);
+
+        int actual = presentation.calculateBenefit(order);
+
+        assertThat(actual).isEqualTo(0);
     }
 
     @Test
