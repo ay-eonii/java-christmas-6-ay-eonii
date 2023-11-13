2731eaa5e114a57d6cf270e4c2e34ee7a534ebe7
ay-eonii <naayen@naver.com>
2023-11-14 01:12:38 +0900

    test(customBenefit): 혜택 계산 테스트

---
+++ src/test/java/christmas/domain/benefit/CustomBenefitTest.java
@@ -0,0 +1,49 @@
+package christmas.domain.benefit;
+
+import christmas.domain.Order;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CustomBenefitTest {
+    CustomBenefit customBenefit;
+    Order order;
+
+    @BeforeEach
+    void setUp() {
+        customBenefit = new CustomBenefit(3);
+        order = new Order("해산물파스타-2,레드와인-1,초코케이크-1");
+    }
+
+    @DisplayName("혜택을 계산한다.")
+    @Test
+    void checkBenefit() {
+        customBenefit.checkBenefit(order);
+
+        String result = customBenefit.toString();
+        assertThat(result).contains(
+                "평일 할인: -2023원",
+                "크리스마스 디데이 할인: -1200원",
+                "증정 이벤트: -25000원",
+                "특별 할인: -1000원"
+        );
+    }
+
+    @Test
+    void getPresentation() {
+    }
+
+    @Test
+    void getTotalBenefit() {
+    }
+
+    @Test
+    void getExpectedPay() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
